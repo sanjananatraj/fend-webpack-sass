248228264afe6215a7a3f9895fd0e66d6f62133d
Sanjana Natraj <sanjana.natraj319@gmail.com>
2021-03-08 16:40:38 -0800

    used weatherapi with fetch request to post weather data

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,18 +1,41 @@
+// function handleSubmit(event) {
+//     event.preventDefault()
+
+//     // check what text was put into the form field
+//     let formText = document.getElementById('name').value
+
+//     Client.checkForName(formText)
+
+//     console.log("::: Form Submitted :::")
+//     fetch('http://localhost:8081/test')
+//     .then(res => {
+//         return res.json()
+//     })
+//     .then(function(data) {
+//         document.getElementById('results').innerHTML = data.message
+//     })
+// }
+
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const key = 'f7109e7cf4bd6667584b2b3bd939a860';
+
 function handleSubmit(event) {
     event.preventDefault()
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-
-    Client.checkForName(formText)
-
+    let zip = document.getElementById('name').value
+    console.log("ZIP CODE: ", zip)
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+
+    const url = `${baseURL}${zip}&appid=${key}`;
+
+    fetch(url)
     .then(res => {
         return res.json()
     })
     .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
+        console.log(data);
+        document.getElementById('results').innerHTML += data.name + "<br>";
+        document.getElementById('results').innerHTML += data.main.temp + "<br>";
     })
 }
 
